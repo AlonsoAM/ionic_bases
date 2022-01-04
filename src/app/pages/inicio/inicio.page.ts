463390f0016d57dea9e729fc93fad18dffac4770
Alonso Anchante Moreno <alonso.amoreno@live.com>
2022-01-04 14:00:56 -0500

    Implementr un menú

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
-}
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-inicio',
@@ -12,80 +10,12 @@ interface Componente {
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  componentes: Componente[] = [
-    {
-      icon: 'leaf-outline',
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet',
-    },
-    {
-      icon: 'alert-circle-outline',
-      name: 'Alert',
-      redirectTo: '/alert',
-    },
-    {
-      icon: 'person-outline',
-      name: 'Avatar',
-      redirectTo: '/avatar',
-    },
-    {
-      icon: 'rocket-outline',
-      name: 'Buttons',
-      redirectTo: '/botons',
-    },
-    {
-      icon: 'card-outline',
-      name: 'Cards',
-      redirectTo: '/card',
-    },
-    {
-      icon: 'checkmark-done-outline',
-      name: 'CheckBox',
-      redirectTo: '/check',
-    },
-    {
-      icon: 'calendar-outline',
-      name: 'DateTime',
-      redirectTo: '/date-time',
-    },
-    {
-      icon: 'flame-outline',
-      name: 'Fab',
-      redirectTo: '/fab',
-    },
-    {
-      icon: 'grid-outline',
-      name: 'Grid',
-      redirectTo: '/grid',
-    },
-    {
-      icon: 'infinite-outline',
-      name: 'Infinite Scroll',
-      redirectTo: '/infinit',
-    },
-    {
-      icon: 'albums-outline',
-      name: 'Input Forms',
-      redirectTo: '/input',
-    },
-    {
-      icon: 'list-outline',
-      name: 'List - Sliding',
-      redirectTo: '/list',
-    },
-    {
-      icon: 'reorder-three-outline',
-      name: 'List - Reorder',
-      redirectTo: '/list-reorder',
-    },
-    {
-      icon: 'reload-circle-outline',
-      name: 'Loading',
-      redirectTo: '/loading',
-    },
-  ];
+  // componentes: Componente[] = [];
+  componentes: Observable<Componente[]>;
 
-  constructor() {}
+  constructor(private dataServise: DataService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.componentes = this.dataServise.getMenuOpts();
+  }
 }
