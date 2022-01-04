aa963bde5bc4597bf092b6a6889aba2711232e68
Alonso Anchante Moreno <alonso.amoreno@live.com>
2022-01-04 00:19:09 -0500

    Agregando el infinite Scroll

--- src/app/pages/infinit/infinit.page.ts
+++ src/app/pages/infinit/infinit.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-infinit',
@@ -9,9 +10,30 @@ export class InfinitPage implements OnInit {
 
   data = Array(20);
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild(IonInfiniteScroll) infiniteScroll;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loadData() {
+    // console.log(event);
+    setTimeout(() => {
+
+      if (this.data.length > 50) {
+        this.infiniteScroll.complete();
+        this.infiniteScroll.disabled = true;
+        return;
+      }
+
+      const newvoArr = new Array(20);
+      this.data.push(...newvoArr);
+
+      // event.target.complete();
+      this.infiniteScroll.complete();
+    }, 1500);
   }
 
 }
